69b651b7ea8499dbdd873c0b577f5f127f73c1aa
Shashank Srivastava <114511335+srivass2@users.noreply.github.com>
2025-03-17 15:40:24 +0000

    datatypes and variable added

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 123443;
+let emailId = "abcd@yahoo.com";
+var accountPassword = "12345"
+accountCity = "Lucknow"  // not a recomended way to initialize without dataType
+let accountState; // by default if not initialized then it has undefined value.
+
+//accountId = 223344    // not allowed since it is const type.
+
+/*
+Donot use (var) dataType anymore. Because of the issue in block scope and functional scope
+*/
+
+emailId = "ss@gmail.com"
+accountPassword = "98765";
+accountCity = "Dublin"
+
+console.table([accountId, emailId, accountPassword, accountCity, accountState]);
+
+console.log(accountId)
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+/*
+"use strict";  
+
+This command tell the engine that treat all JS code as newer version.
+once this is written we cannot go to previous state.
+Now we dont use this frequently but a good thing to know.
+*/ 
+
+//DataTypes
+
+let name = "Shashank";  // string type
+let age = 34            // number type
+let isLoggedIn = true   // boolean type
+let state = null        // null type
+
+            /*primitive datatype*/
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null =>standalone value
+//undefined => value not defined
+//symbol =>unique
+
+            /*non primitive datatype*/
+//object
+
+console.log(typeof undefined); // it is undefined type 
+console.log(typeof null); // it is object type
