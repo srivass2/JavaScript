c0badc37a7ba174baa02b1b8ba053f638785d907
Shashank Srivastava <114511335+srivass2@users.noreply.github.com>
2025-03-26 15:31:04 +0000

    date

--- 01_Basics/06_date.js
+++ 01_Basics/06_date.js
@@ -1,2 +1,13 @@
 let myDate = new Date();
 console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof(myDate));
+
+let myTimestamp = new Date();
+console.log(myTimestamp.getTime()); //the value returned is in Milliseconds
+console.log(myTimestamp.getTime()/1000); //this will give timestamp in seconds
+console.log(Math.floor(myTimestamp.getTime()/1000)); // to avoid the value in decimal we have used Math.floor() method.
+
+
