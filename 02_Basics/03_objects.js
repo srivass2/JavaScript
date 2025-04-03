0155e3affb00f49d8b43856d9155cddec3f0f25c
Shashank Srivastava <114511335+srivass2@users.noreply.github.com>
2025-04-03 13:51:35 +0000

    object part 1

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -3,17 +3,25 @@
 
 
 
-
-
 // object Literals 
+
+const mySym = Symbol();
+
 const jsUser = {
     name : "Shashank",
     "full name" : "Shashank Srivastava",
     age : 18,
+    [mySym] : "myKey1",
     location : "Heuston",
     email : "shashank@google.com",
     isLoggedIn : false,
-    lastLogInDays : ["Monday", "Wednessday"]
+    lastLogInDays : ["Monday", "Wednessday"],
+    greeting : function () {
+        console.log("Hello JS");
+    },
+    greetingTwo : function() {
+        console.log(`Hello JS user ${this.name}`);
+    }
 
 }
 
@@ -21,3 +29,24 @@ console.log(jsUser.email);  // generally we access object by this way.
 
 console.log(jsUser["email"]); // INTERSTING... This is Another way to access the object. Here we have used [""] because we are accessing it as a string. Since in the background the keys of object are stored as String with ""
 console.log(jsUser["full name"]); // this expalin the above reasoning.
+
+console.log(jsUser[mySym]); // access the symbol type.
+
+jsUser.email = "shashank@chatgpt.com";
+console.log(jsUser.email); // overreide value of a key in Object.
+
+
+//Object.freeze(jsUser); // we can freeze the object and values cannot be changed.
+jsUser.email = "shashank@microsoft.com";
+console.log(jsUser.email);
+
+console.log(jsUser);
+
+// We can also define functions inside the object and it is treated as normal any other elements in the object.
+console.log(jsUser);
+
+console.log(jsUser.greeting);
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
+
+// IMPORTANT.... mostly we access object using (.) but ONLY in some cases we use [""] or [](incase of Symbols)
