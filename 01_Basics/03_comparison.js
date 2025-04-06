e313bb2a75e1c1263bd69f2f781f8b27ba7b9d19
Shashank Srivastava <114511335+srivass2@users.noreply.github.com>
2025-04-06 11:40:47 +0000

    updated comment

--- 01_Basics/03_comparison.js
+++ 01_Basics/03_comparison.js
@@ -5,7 +5,7 @@ console.log(null >= 0);
 /* It is recommended to compare between same dataType variable.
 In this case the equality check like == and comparisons like > < >= <=  work diffently.
 Comparisons convert null to a number, treating null as 0 that is why in (3) when null is compared to 0 it gives TRUE. because null is converted to number and it becomes 0. like wise (1) also gave FALSE since 0 > 0 is false.
-WHERE AS for equality it doesnot convert hence in (1) where null is not equal to 0 so it gave FALSE. */
+WHERE AS for equality it doesnot convert hence in (2) where null is not equal to 0 so it gave FALSE. */
 
 console.log(undefined < 0);
 console.log(undefined == 0);
