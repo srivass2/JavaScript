086a7ad075230b9070c5ea0fc08ac4b46b5c33a1
Shashank Srivastava <114511335+srivass2@users.noreply.github.com>
2025-04-15 14:12:55 +0000

    scope continued

--- 03_Basics/03_scope.js
+++ 03_Basics/03_scope.js
@@ -14,3 +14,51 @@ console.log(b);
 //console.log(d); // throws error at this which is correct since (d) has local scope within if block
 
 //+++ INTERESTING FACT  global scope in browser console is different from global scope used in node(code)
+
+
+// Scope in functions
+
+function one() {
+    const username = "Shashank";
+    function two() {
+        const website = "Youtube";
+        console.log(username); // this got printed because it acted as global variable for function two().
+    }
+    //console.log(website); // throw error here since website is local for function two hence cannot be accessed outside it.
+    two();
+}
+one();
+//console.log(username); // where as when we access it outside function one() it gives error coz the variable had scope till inside the Function.
+
+// scope inside if condition
+
+if(true) {
+    const username = "shashank";
+    if (username === "shashank") {
+        const website = "youtube";
+        console.log(`${username} ${website}`)
+    }
+    //console.log(website); // error expected since the variable is used outside the scope.
+}
+//console.log(username); // error expected since the variable is used outside the scope.
+
+
+//+++++++++++++++++++++++++ INTERESTING FACT ++++++++++++++++++++++++++++++++++++++
+
+addOne(5);
+function addOne(num) {
+    return num + 1;
+}
+
+//addTwo(6);
+const addTwo = function(num){
+    return num + 2;
+}
+addTwo(6);
+
+// Above code runs fine even although addOne function is called before function definition but if we call addTwo function before the addTwo function definition it will throw error BECAUSE addTwo function is initialised differently, if it was the same way as addOne function then no error e.g below code for addThree.
+
+addThree(6);
+function addThree(num){
+    return num + 3;
+}
