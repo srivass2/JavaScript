52a353066f2c330841751b4a8e016f1eb9cc53d0
Shashank Srivastava <114511335+srivass2@users.noreply.github.com>
2025-04-15 12:01:16 +0000

    function continued

---
+++ 03_Basics/02_functionContinued.js
@@ -0,0 +1,35 @@
+//+++++++++++++++++++++ Use of Rest(...) and spread(...) operator in function ++++++++++++++++++++++
+// both looks same but the difference is in their usage.
+
+function calculateCartItem(num1) {
+
+    return num1;
+}
+console.log(calculateCartItem(200));
+
+// In real case scenario we add multiple items in cart so how to pass that in the funtion as argument since we cannot know how many variable we need on run time.
+
+function calculateCartItemRestOperator(...num1) { // by using rest(...) operator we can achieve it.
+
+    return num1;
+}
+console.log(calculateCartItemRestOperator(200, 400, 500, 20000));
+
+// how to access object inside a function
+
+const user = {
+    username : "Shashank",
+    price : 200
+}
+function handleObject(anyobject) {
+    console.log(`The user's name is ${anyobject.username} and the price is ${anyobject.price}`);
+}
+handleObject(user);
+
+// how to access arrray inside a function
+
+const newArray = [100, 200, 300, 400];
+function getSecondArray(getArray) {
+    return getArray[1];
+}
+console.log(`Second element in the array is ${getSecondArray(newArray)}`)
