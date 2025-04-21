82bd7bccbe13f3e59b9ea7ebea3705de1bac41f9
Shashank Srivastava <114511335+srivass2@users.noreply.github.com>
2025-04-21 14:06:40 +0000

    IIFE function in detail

--- 03_Basics/05_IIFE.js
+++ 03_Basics/05_IIFE.js
@@ -1,7 +1,40 @@
+// variable scope in IIFE function
 (() => {
     let message = "hello";
     console.log(message);
 }) ();
+//console.log(message);
 
-let message = "shashank";
-console.log(message);
+// variable scope in Normal function
+function fun() {
+let message1 = "shashank";
+console.log(message1);
+}
+//console.log(message1);
+
+/* In both the above functions, scope of variable is same i.e both variable cannot be accessed out the function but the difference lies below:
+
+1) IIFE(Immediately Invoked Function Expression) is a function that get executed immediately without calling it later. e.g You define a function, and instead of saving it for later, you run it right away—like cooking and eating instantly instead of storing it.
+
+2) IIFE function doesnot have a name so also one of the reason why executed immeditely.
+
+3) It also prevents from global scope variable  pollution.
+
+Summary of Difference
+
+Feature	                          Regular Function	              IIFE
+Needs a name?	                    Yes (usually)	               No
+Needs to be called?	                Yes	                           No — calls itself automatically
+Scope isolation?	                Yes	                           Yes
+When does it run?	                When you call it	           Right away after it's defined
+
+
+Ques. So if the variable scope wise both IIFE and normal functions are same then how IIFE is helping in prevention from  global space pollution ?
+
+Ans. IIFE is used when you:Don’t need to reuse the function. Just want to do something quickly, once. Want to keep your code clean, by avoiding:Declaring extra function names or Forgetting to call the function.
+IIFE has No global function name. One-time setup done instantly. Nothing leaks out into global space — even the function name is gone!
+
+Inshort, IIFE: perfect for one-time tasks that you want to run immediately and keep everything inside private — no names, no clutter, no pollution.
+
+for example: DB connection.
+*/
