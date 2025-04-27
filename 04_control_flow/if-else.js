c3b1ffb5a9d5273b074a8c8d4ab89cd4e88c21f7
Shashank Srivastava <114511335+srivass2@users.noreply.github.com>
2025-04-27 14:48:22 +0000

    if-else basic

---
+++ 04_control_flow/if-else.js
@@ -0,0 +1,19 @@
+// If else condtion works same like any other language. 
+
+/*
+if() {
+}
+else {
+}
+
+ab = true
+cd = true
+ef = true
+if(ab && cd && ef) {
+console.log("inside if")
+}
+
+Video continue from 24.43
+*/
+
+
