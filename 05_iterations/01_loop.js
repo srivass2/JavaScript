cd69b6341faecf0d8c422933d1dd5acf4d49193f
Shashank Srivastava <114511335+srivass2@users.noreply.github.com>
2025-04-28 14:14:51 +0000

    loop

---
+++ 05_iterations/01_loop.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    console.log(element);    
+}
+for (let j = 0; j <= 10; j++) {
+    const element = j;
+    if (j == 5) {
+        console.log("5 is the best number");        
+    }
+    console.log(element);    
+}
