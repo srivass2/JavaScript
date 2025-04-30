a9d47a0d224295eacc3b06d365525836fc9854e4
Shashank Srivastava <114511335+srivass2@users.noreply.github.com>
2025-04-30 15:30:18 +0000

    loops

--- 05_iterations/01_loop.js
+++ 05_iterations/01_loop.js
@@ -2,6 +2,7 @@ for (let i = 0; i <= 10; i++) {
     const element = i;
     console.log(element);    
 }
+
 for (let j = 0; j <= 10; j++) {
     const element = j;
     if (j == 5) {
@@ -9,3 +10,38 @@ for (let j = 0; j <= 10; j++) {
     }
     console.log(element);    
 }
+
+for (let i = 0; i < 10; i++) {
+    console.log(`Outer loop: ${i}`);
+    for (let j = 0; j < 10; j++) {
+        console.log(`Inner loop: ${j}`);        
+    }        
+}
+
+const array = ["sam", "tom", "james"];
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(`array name : ${element}`);    
+    
+}
+
+for (let index = 1; index <= 20; index++) {    
+    if (index === 5) {
+        console.log(`Detected 5`);
+        break;         
+    }
+    console.log(`Value of index: ${index}`);
+        
+}
+
+for (let index = 1; index <= 20; index++) {    
+    if (index === 5) {
+        console.log(`Detected 5`);
+        continue;         
+    }
+    console.log(`Value of index: ${index}`);
+         
+}
+/* break keyword makes the control comes out of the loop the moment the condition is fulfilled and skips the rest of execution.
+WhereAs continue keyword skips the execution for just once when the condition is met but it doesnot come out of loop and allows the rest execution of the code..
+*/
