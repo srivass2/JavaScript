6aee9ff9383fb2bc82ba36f1be55d55eb1333b08
Shashank Srivastava <114511335+srivass2@users.noreply.github.com>
2025-04-30 16:16:51 +0000

    for-of loop

---
+++ 05_iterations/03_highOrderLoop.js
@@ -0,0 +1,48 @@
+// for-of loop can be used for string, arrays, maps but not for OBJECTS.
+let arr = [1, 2, 3, 4, 5]
+for (const num of arr) {
+    console.log(`number : ${num}`);   
+    
+}
+
+let greeting = "Hello World!";
+for (const greet of greeting) {
+    console.log(`each charracter is ${greet}`);    
+    
+}
+
+//+++++++++++++++++++++++++++++++ MAP +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/* 
+Maps hold Key value pair.
+It remembers the original insertion order of the keys.
+It doesnot have duplicate values. only uniques allowed.
+*/
+
+const map = new Map();
+map.set('IN', "INDIA")
+map.set('Fr', "France")
+map.set('IN', "INDIA") // since map so duplicate values are not allowed.It will display only unique
+console.log(map);
+
+for (const key of map) {
+    console.log(key);
+    
+}// Here we see that it is displayed in a form of multiple arrays ['IN', 'INDIA] [ 'Fr', 'France' ]
+// To avoid this we can destructure it by simple adding key and value. e.g:
+
+
+for (const [keys, values] of map) {
+    console.log(keys, values);
+}
+
+// for-of loop in case of OBJECTS to see that it cannot be used.
+
+const obj = {
+    game1: "NFS",
+    game2: "Counter Strike"
+}
+for (const [key1, value1] of obj) {
+    console.log(key1 , value1);
+    
+}
