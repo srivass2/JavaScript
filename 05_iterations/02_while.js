59cffdcde64724bc8cc3fb291bccb5aab7ee8060
Shashank Srivastava <114511335+srivass2@users.noreply.github.com>
2025-04-30 16:16:34 +0000

    while

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,11 @@
+let index = 0;
+while (index <= 10) {
+    console.log(`value of index ${index}`);
+    index = index + 2;    
+}
+
+let score = 1
+do {
+    console.log(`socre is ${score}`);
+    score = score + 1;
+} while (score <= 10);
